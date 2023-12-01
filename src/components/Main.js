563cee8d8de54dcfa6c324e6bb5527faada24768
kiboma <omayioben@gmail.com>
2023-12-01 09:31:31 +0300

    Add toggle button to hide and show all books

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,19 +2,29 @@ import { useState } from "react";
 
 const Main = () => {
     const [books, setBooks]=useState([
-        {id:123, name:"Shreds of tenderness", description:"lorem ipsum"},
-        {id:124, name:"Shreds of tenderness", description:"lorem ipsum"},
-        {id:125, name:"Shreds of tenderness", description:"lorem ipsum"},
-        {id:126, name:"Shreds of tenderness", description:"lorem ipsum"},
-        {id:127, name:"Shreds of tenderness", description:"lorem ipsum"},
+        {id:123, name:"Shreds of tenderness", description:"lorem ipsum", completed:true},
+        {id:124, name:"Shreds of tenderness", description:"lorem ipsum", completed:false},
+        {id:125, name:"Shreds of tenderness", description:"lorem ipsum", completed:false},
+        {id:126, name:"Shreds of tenderness", description:"lorem ipsum", completed:true},
+        {id:127, name:"Shreds of tenderness", description:"lorem ipsum", completed:false},
     ]);
     
+    const [show, setShow]=useState(true);
+
     function handleDelete(id){
         console.log("Clicked",id);
         setBooks(books.filter(book => book.id != id));
     }
+
+    function handleToggle(){
+        setShow(!show);
+    }
   return (
-    <section>
+    <section className="main">
+        <div className="toogle-btn">
+            <button onClick={handleToggle}>Toogle</button>
+        </div>
+        
         <table>
             <thead>
                 <tr>
@@ -25,7 +35,7 @@ const Main = () => {
                 </tr>
             </thead>
             <tbody>       
-            {books.map(book =>
+            {show && books.map(book =>
                 <tr key={book.id}>
                     <td>{book.id}</td>
                     <td>{book.name}</td>
