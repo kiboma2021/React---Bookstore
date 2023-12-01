f9ab05a7329458d59c9e5eed3f4820140fb21db9
kiboma <omayioben@gmail.com>
2023-12-01 07:59:21 +0300

    Add footer and use inline styling

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <section>
+      <p style={{display:"flex", justifyContent:"center", fontSize:"1.5rem"}}>@2023 - All rights reserved</p>
+    </section>
+  )
+}
+
+export default Footer
