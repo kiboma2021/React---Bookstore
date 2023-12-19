851f1c402ea69086f4b0f80da07a91fd126fd57a
kiboma <omayioben@gmail.com>
2023-12-19 14:26:13 +0300

    Add BrowserRouter and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { useState,useEffect } from 'react';
+import { Routes,Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
+import Contact from './components/Contact';
+import Profile from './components/Profile';
 
 import './App.css';
+
 
 function App() {
   const [books, setBooks]=useState(JSON.parse(localStorage.getItem("books"))||[]);
@@ -16,7 +20,13 @@ function App() {
   return (
     <div className="App">
       <Header books={books} />
-      <Main books={books} setBooks={setBooks} />
+      <Routes>
+        <Route path='/' element={<Main books={books} setBooks={setBooks} />}> </Route>
+        <Route path='contact' element={<Contact />} ></Route>
+        <Route path='profile' element={<Profile />}></Route>
+
+      </Routes>
+      
 
 
       <Footer />
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
@@ -7,7 +8,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
